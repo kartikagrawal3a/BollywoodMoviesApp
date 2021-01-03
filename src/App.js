d95c38245d68fef7f8bd6922add373f1492bb13a
Kartik Agrawal <kartik.agrawal1_cs18@gla.ac.in>
2021-01-03 23:26:37 +0530

    Modification 2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App(){
           <Card imgsrc = {Data[1].imgsrc} nameFilm = {Data[1].nameFilm} link = {Data[1].link} />
           <Card imgsrc = {Data[2].imgsrc} nameFilm = {Data[2].nameFilm} link = {Data[2].link} />
         </div>
+        <div className = "row">
+          <Card imgsrc = {Data[3].imgsrc} nameFilm = {Data[3].nameFilm} link = {Data[3].link} />
+          <Card imgsrc = {Data[4].imgsrc} nameFilm = {Data[4].nameFilm} link = {Data[4].link} />
+          <Card imgsrc = {Data[5].imgsrc} nameFilm = {Data[5].nameFilm} link = {Data[5].link} />
+        </div>
       </div>
     </>
   );
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,6 +1,9 @@
 import Laxmi from './images/laxmi.jpg';
 import Dilbechara from './images/dilbechara.jpg';
 import Coolie from './images/coolie.jpg';
+import Baaghi from './images/baaghi.jpg';
+import Khuda from './images/khuda.jpg';
+import Gunjan from './images/gunjan.jpeg';
 
 const Data = [
     {
@@ -17,6 +20,21 @@ const Data = [
         imgsrc : `${Coolie}`,
         nameFilm : 'Coolie No. 1',
         link : 'https://www.primevideo.com/detail/amzn1.dv.gti.eab9f6f3-29da-6529-a314-9a6c4e1224e9/?ref_=dvm_pds_gen_in_as_s_gt_coolie|m_wu2weBQcc_c488321020069&gclid=Cj0KCQiA88X_BRDUARIsACVMYD-i-jOVMLhPt-0BdnHBXyBE9nY5TaHpodq8PK_yMkGHBCH0YFnjyNYaAhFWEALw_wcB'
+    },
+    {
+        imgsrc : `${Baaghi}`,
+        nameFilm : 'Baaghi 3',
+        link : 'https://www.hotstar.com/in/movies/baaghi-3/1260025583/watch?utm_source=gwa'
+    },
+    {
+        imgsrc : `${Khuda}`,
+        nameFilm : 'Khuda Hafiz',
+        link : 'https://www.hotstar.com/in/movies/khuda-haafiz/1260036204/watch?utm_source=gwa'
+    },
+    {
+        imgsrc : `${Gunjan}`,
+        nameFilm : 'Gunjan Saxena',
+        link : 'https://www.netflix.com/in/title/81292944?source=35'
     }
 ];
 
